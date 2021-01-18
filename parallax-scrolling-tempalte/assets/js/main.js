dd3c2d247fb7a72148878cfcaaeea48bd5b1bfda
miureus <miureus@gmail.com>
2021-01-18 13:51:29 +0700

    upload parallax-scrolling-template

---
+++ parallax-scrolling-tempalte/assets/js/main.js
@@ -0,0 +1,40 @@
+// MENU
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// RELLAX
+var rellax = new Rellax('.parallax');
+
+// GSAP
+/*Nav*/
+gsap.from('.nav_logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
+gsap.from('.nav_toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
+gsap.from('.nav_item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+
+/*Text*/
+gsap.from('.home_title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
+gsap.from('.home_subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+
+/*Scroll*/
+gsap.from('.home_scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
+
+// SCROLL REVEAL SECTION
+const sr = ScrollReveal({
+    duration: 2500,
+    reset: true
+});
+
+/*Data*/
+sr.reveal('.section_data',{origin: 'left',distance: '70px'}); 
+
+/*Imgs*/
+sr.reveal('.section_img',{origin: 'left',distance: '90px',delay: 200}); 
