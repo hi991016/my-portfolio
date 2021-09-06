a2d0b6c79b451d4b45a4c275fd102936a9d43ae9
hi991016 <miureus@gmail.com>
2021-09-06 18:26:20 +0700

    commit brand-landing-page

---
+++ brand-landing-page/assets/js/main.js
@@ -0,0 +1,118 @@
+const timeline = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
+
+timeline.from(".load-text span", {
+  duration: 1.5,
+  delay: 0.5,
+  skewX: -10,
+  skewY: 10,
+  stagger: {
+    amount: 0.4,
+  },
+  y: 50,
+  x: -20,
+  opacity: 0,
+});
+
+timeline.to(".load-text span", {
+  duration: 1.5,
+  delay: 2,
+  skewX: 10,
+  skewY: -10,
+  stagger: {
+    amount: 0.4,
+  },
+  y: -50,
+  x: 20,
+  opacity: 0,
+});
+
+timeline.to(
+  ".loader",
+  {
+    delay: 0.5,
+    duration: 1.5,
+    opacity: 0,
+  },
+  "-=.3"
+);
+
+timeline.to(".loader", {
+  zIndex: -1,
+  duration: 0.1,
+});
+
+timeline.from(".main-text", {
+  duration: 1.2,
+  opacity: 0,
+  skewY: 10,
+  y: 100,
+  stagger: {
+    amount: 0.4,
+  },
+});
+
+timeline.from(".reel", {
+  opacity: 0,
+  duration: 2,
+});
+
+// scroll trigger
+gsap.from(".about-text", {
+  scrollTrigger: {
+    trigger: ".about-text",
+    toggleActions: "restart pause none pause",
+  },
+  y: 100,
+  opacity: 0,
+  duration: 0.8,
+});
+
+gsap.from(".about-desc p, .getintouch", {
+  scrollTrigger: {
+    trigger: ".about-desc p, .getintouch",
+    toggleActions: "restart pause none pause",
+  },
+  y: 100,
+  skewY: 10,
+  opacity: 0,
+  delay: 0.3,
+  duration: 1,
+  stagger: {
+    amount: 0.5,
+  },
+});
+
+gsap.from(".about-hero", {
+  scrollTrigger: {
+    trigger: ".about-hero",
+    toggleActions: "restart none none none",
+  },
+  height: 0,
+  opacity: 0,
+  delay: 0.8,
+  duration: 1.5,
+});
+gsap.from(".reel2", {
+  scrollTrigger: {
+    trigger: ".reel2",
+    toggleActions: "restart pause reverse pause",
+  },
+  y: -200,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".main-text2", {
+  scrollTrigger: {
+    trigger: ".main-text2",
+    toggleActions: "restart pause reverse pause",
+  },
+  y: 100,
+  skewY: 10,
+  opacity: 0,
+  duration: 1.2,
+
+  stagger: {
+    amount: 0.4,
+  },
+});
