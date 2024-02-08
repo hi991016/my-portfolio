ca3d266d4f2bced9405af419adf1de4d84eca999
hi991016 <miureus@gmail.com>
2024-02-08 11:31:12 +0700

    updated file

--- portfolio-template-1/assets/js/main.js
+++ portfolio-template-1/assets/js/main.js
@@ -1,58 +1,114 @@
-/*===== MENU SHOW =====*/
+// SHOW MENU
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
     if(toggle && nav) {
         toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
+            nav.classList.toggle('show-menu')
         })
     }
 }
-showMenu('nav-toogle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU (BỎ GẠCH DƯỚI KHI CLICK MENU KHÁC) =====*/
-const navLink = document.querySelectorAll('.nav_link')
+showMenu('nav-toggle','nav-menu')
+
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-    //Active link
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
-
-    //Remove menu mobile (click vào menu khác sẽ tắt danh sách menu)
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    navMenu.classList.remove('show-menu')
 }
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+// SCROLL SECTION ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true,
-})
+function scrollActive() {
+    const scrollY = window.pageYOffset
+    
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop -50
+        sectionId = current.getAttribute('id')
 
-/*===== SCROLL HOME =====*/
-sr.reveal('.home_title',{})
-sr.reveal('.button',{delay: 200})
-sr.reveal('.home_img',{delay: 400})
-sr.reveal('.home_social-icon',{interval: 200})
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
-/*===== SCROLL ABOUT =====*/
-sr.reveal('.about_img',{})
-sr.reveal('.about_subtitle',{delay: 200})
-sr.reveal('.about_text',{delay: 400})
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header')
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll',scrollHeader)
 
-/*===== SCROLL SKILLS =====*/
-sr.reveal('.skills_subtitle',{})
-sr.reveal('.skills_text',{delay: 200})
-sr.reveal('.skills_data',{interval: 200})
-sr.reveal('.skills_img',{delay: 400})
+// SHOW SCROLLTOP
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll')
+    else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
-/*===== SCROLL WORK =====*/
-sr.reveal('.work_img',{interval: 200})
+// MIXITUP FILTER PORTFOLIO
+// https://www.kunkalabs.com/mixitup/docs/get-started/
 
-/*===== SCROLL CONTACT =====*/
-sr.reveal('.contact_input',{interval: 200})
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+// LINK ACTIVE PORTFOLIO
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio() {
+    if(linkPortfolio) {
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
+
+// SWIPER CAROUSEL
+// https://swiperjs.com/get-started
+const mySwiper = new Swiper('.testimonial__container', {
+    // Optional parameters
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
+
+// GSAP ANIMATION
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x:60})
+gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y:25, ease: 'expo.out', stagger: .2})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y:25, ease: 'expo.out', stagger: .2})
+
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y:25, ease: 'expo.out', stagger: .2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2.3, y:25, ease: 'expo.out', stagger: .2})
